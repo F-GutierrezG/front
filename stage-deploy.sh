f44c54bb3dd4fe6a437fdccd0bc3a0c97605cd0a
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-15 15:39:17 -0300

    CI test

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -4,10 +4,10 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker login -u gitlab-c
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network create client-front-network'
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container stop front'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker container stop $FRONT"
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker container rm front'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker container rm $FRONT"
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker image rm $REGISTRY_REPO/$FRONT:$TAG"
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -p 80:3000 --name client-front --network client-front-network registry.gitlab.com/gusisoft/onelike/client/front/$FRONT:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -p 3000:3000 --name client-front --network client-front-network registry.gitlab.com/gusisoft/onelike/client/front/$FRONT:$TAG"
