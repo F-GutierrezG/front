11ba2a959d0252b3f5f9594e66b3c4d21c38e07d
Francisco Gutierrez <fgutierrez@grouponlatam.com>
2018-11-21 17:54:07 -0300

    static ips for containers

--- stage-deploy.sh
+++ stage-deploy.sh
@@ -2,7 +2,7 @@
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker login -u gitlab-ci-token -p $DOCKER_TOKEN registry.gitlab.com"
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network create client-front-network'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network create --subnet=172.19.0.0/16 client-front-network'
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker container stop $FRONT"
 
@@ -10,6 +10,6 @@ ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker container rm $FRO
 
 ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker image rm $(docker images registry.gitlab.com/gusisoft/onelike/client/front/client-front -q)'
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -p 3000:3000 --name client-front --network client-front-network registry.gitlab.com/gusisoft/onelike/client/front/$FRONT:$TAG"
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} "docker run -d -p 3000:3000 --name client-front --network client-front-network --ip 172.19.0.2 $REGISTRY_REPO/$FRONT:$TAG"
 
-ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect client-network client-front'
+ssh -o StrictHostKeyChecking=no ubuntu@${STAGE_SERVER} 'docker network connect client-network --ip 172.18.0.5 client-front'
