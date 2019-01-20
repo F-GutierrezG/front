c14a8f184eb1147d60fc6da532a5707e19031a66
Carlos Silva <anklosr@gmail.com>
2019-01-20 11:22:56 -0300

    permite visualizar la fecha de expiration en listado y poder modificar en formulario

--- src/Pages/ManageUsers.jsx
+++ src/Pages/ManageUsers.jsx
@@ -6,14 +6,16 @@ import Users from "Containers/ManageUsers";
 const ManageUsers = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   let listUsersURL = `${process.env.REACT_APP_COMPANIES_SERVICE_URL}/users`;
+  let createUserURL = `${process.env.REACT_APP_COMPANIES_SERVICE_URL}/users`;
 
   if (user.admin) {
     listUsersURL = `${process.env.REACT_APP_USERS_SERVICE_URL}/admins`;
+    createUserURL = `${process.env.REACT_APP_USERS_SERVICE_URL}/admins`;
   }
   return (
     <Users
       listUsersURL={listUsersURL}
-      createUserURL={process.env.REACT_APP_USERS_SERVICE_URL}
+      createUserURL={createUserURL}
       deactivateUserURL={process.env.REACT_APP_USERS_SERVICE_URL}
       activateUserURL={process.env.REACT_APP_USERS_SERVICE_URL}
       downloadURL={`${process.env.REACT_APP_EXPORTER_SERVICE_URL}/admins`}
